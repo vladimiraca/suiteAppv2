2e4840c3cbdf204d2d0fc6293feb300363da01e8
Vladimir Aca <vladimir.aca@versapay.com>
2022-12-03 16:23:15 -0500

    new object added

---
+++ src/FileCabinet/SuiteApps/com.versapay.suiteappv2/versapay_formtestv_sl.js
@@ -0,0 +1,23 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ * @Company Versapay
+ * @Description this is a test suitelet script for DEMO
+ */
+define([],
+    
+    () => {
+        /**
+         * Defines the Suitelet script trigger point.
+         * @param {Object} scriptContext
+         * @param {ServerRequest} scriptContext.request - Incoming request
+         * @param {ServerResponse} scriptContext.response - Suitelet response
+         * @since 2015.2
+         */
+        const onRequest = (scriptContext) => {
+
+        }
+
+        return {onRequest}
+
+    });
